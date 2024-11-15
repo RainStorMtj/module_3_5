e4a3f8a4221be7b5ce60b01296f3aa4ce663427a
RainStorMtj <145570462+RainStorMtj@users.noreply.github.com>
2024-11-15 15:41:11 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,10 @@
+def get_multiplied_digit(number):
+    str_number = str(number)
+    first = int(str_number[:1])
+    if len(str_number) <= 1:
+        return first
+    else:
+       return first * get_multiplied_digit(int(str_number[1:]))
+
+result = get_multiplied_digit(40203)
+print(result)
