33a523e5ae029229b07128b4e4599bd0c80e04c6
RainStorMtj <145570462+RainStorMtj@users.noreply.github.com>
2024-11-19 01:32:55 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,6 +1,6 @@
 def get_multiplied_digit(number):
     str_number = str(number)
-    first = int(str_number[:1])
+    first = int(str_number[0])
     if len(str_number) <= 1:
         return first
     else:
