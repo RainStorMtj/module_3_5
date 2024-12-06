5496aea13b091b58022ea07a5fdfdd1265b2796f
RainStorMtj <145570462+RainStorMtj@users.noreply.github.com>
2024-12-06 23:53:09 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,14 @@
 def get_multiplied_digit(number):
     str_number = str(number)
     first = int(str_number[0])
-    if len(str_number) <= 1:
-        return first
+    if len(str_number) > 1:
+        return first * get_multiplied_digit(int(str_number[1:]))
+    if first == 0:
+        return 1
     else:
-       return first * get_multiplied_digit(int(str_number[1:]))
+       return first
 
 result = get_multiplied_digit(40203)
+result2 = get_multiplied_digit(402030)
 print(result)
+print(result2)
